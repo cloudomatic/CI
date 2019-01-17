fb813af45fab166351b9f4f3342e64e8e8b29b3e
EC2 Default User <ec2-user@ip-172-31-45-39.us-west-2.compute.internal>
2019-01-17 02:55:12 +0000

    /vars update

--- vars/auditEvent2.groovy
+++ vars/auditEvent2.groovy
@@ -7,7 +7,7 @@ def call(String event) {
  try {
    println("timestamp=" + timestamp);
    def sql = Sql.newInstance("jdbc:postgresql://postgres_host/audit", "ciuser","ciuser", "org.postgresql.Driver")
-   sql.execute "INSERT INTO audit_events (event_datetime, event_uid, event_name) VALUES ('xxx', 'xxx', '" + event + "');"
+   sql.execute "INSERT INTO audit_events (event_datetime, event_uid, event_name) VALUES ('" + timestamp + "', 'unknown_user', '" + event + "');"
  } catch(Exception e) {
    println("auditEvent2(): Exception " + e);
  }
