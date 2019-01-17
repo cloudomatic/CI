828055c09be2d9beb6622b20b2dfee0521e46f1f
EC2 Default User <ec2-user@ip-172-31-45-39.us-west-2.compute.internal>
2019-01-17 02:26:02 +0000

    First test of shared lib

---
+++ vars/auditEvent2.groovy
@@ -0,0 +1,9 @@
+import groovy.sql.Sql
+
+def call(String event) {
+  //echo "Call pgsql here and log the event into the database"
+  //sh "psql -h postgres_host -U postgres SELECT * FROM audit_events;" 
+ def timestamp = new Date().format("yyyyMMdd.HHmm", TimeZone.getTimeZone('EST'))
+ def sql = Sql.newInstance("jdbc:postgresql://postgres_host/audit", "ciuser","ciuser", "org.postgresql.Driver")
+ sql.execute "INSERT INTO audit_events (event_datetime, event_uid, event_name) VALUES ('001', '002', '003')"
+}
