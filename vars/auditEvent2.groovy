a5210fe35f35f5d38e880a3beb6480b9c3215448
EC2 Default User <ec2-user@ip-172-31-45-39.us-west-2.compute.internal>
2019-01-17 02:44:26 +0000

    /vars update

--- vars/auditEvent2.groovy
+++ vars/auditEvent2.groovy
@@ -1,9 +1,14 @@
 import groovy.sql.Sql
 
 def call(String event) {
- echo "Call pgsql here and log the event into the database"
+ echo "auditEvent2(): >"
   //sh "psql -h postgres_host -U postgres SELECT * FROM audit_events;" 
  def timestamp = new Date().format("yyyyMMdd.HHmm", TimeZone.getTimeZone('EST'))
- def sql = Sql.newInstance("jdbc:postgresql://postgres_host/audit", "ciuser","ciuser", "org.postgresql.Driver")
- sql.execute "INSERT INTO audit_events (event_datetime, event_uid, event_name) VALUES ('001', '002', '003')"
+ try {
+   def sql = Sql.newInstance("jdbc:postgresql://postgres_host/audit", "ciuser","ciuser", "org.postgresql.Driver")
+   sql.execute "INSERT INTO audit_events (event_datetime, event_uid, event_name) VALUES ('001', '002', '003')"
+ } catch(Exception e) {
+   println("auditEvent2(): Exception " + e);
+ }
+ echo "auditEvent2(): <"
 }
