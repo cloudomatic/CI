08d535f740d32cdf7a59da4be2552359966fcd96
EC2 Default User <ec2-user@ip-172-31-36-248.us-west-2.compute.internal>
2019-01-18 13:19:54 +0000

    Testing database access

--- vars/auditEvent.groovy
+++ vars/auditEvent.groovy
@@ -1,4 +1,15 @@
+import groovy.sql.Sql
+
 def call(String event) {
-  echo "Call pgsql here and log the event into the database"
-  sh "psql -h postgres_host -U postgres SELECT * FROM audit_events;" 
+ echo "auditEvent2(): >"
+ //sh "psql -h postgres_host -U postgres SELECT * FROM audit_events;" 
+ def timestamp = new Date().format("yyyyMMdd.HHmm", TimeZone.getTimeZone('EST'))
+ try {
+   println("timestamp=" + timestamp);
+   def sql = Sql.newInstance("jdbc:postgresql://postgres_host/audit", "ciuser","ciuser", "org.postgresql.Driver")
+   sql.execute "INSERT INTO audit_events (event_datetime, event_uid, event_name) VALUES ('" + timestamp + "', 'unknown_user', '" + event + "');"
+ } catch(Exception e) {
+   println("auditEvent2(): Exception " + e);
+ }
+ echo "auditEvent2(): <"
 }
