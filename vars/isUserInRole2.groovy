ac991326b4af72042d241de38486456c9396aeb7
Cloudomatic <cloudomatic@github.com>
2019-01-21 16:32:34 -0500

    Updating global library

--- vars/isUserInRole2.groovy
+++ vars/isUserInRole2.groovy
@@ -1,3 +1,5 @@
+import com.michelin.cio.hudson.plugins.rolestrategy.RoleBasedAuthorizationStrategy;
+
 def call() {
   def job = Jenkins.getInstance().getItemByFullName(env.JOB_BASE_NAME, Job.class)
   def build = job.getBuildByNumber(env.BUILD_ID as int)
